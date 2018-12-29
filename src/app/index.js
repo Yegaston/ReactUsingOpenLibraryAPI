92412c30ca2270fd79cb1b545289b18bd769201e
Gaston Graciani <gastongraciani@gmail.com>
2018-12-29 14:58:33 -0300

    True

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+
+import App from './App'
+import data from './data.json';
+
+const headings = ['When', 'Who', 'Description']
+
+render(<App data={data} title='OpenLibrary Api' headings={}/>, document.getElementById('app'));
